0d5468c28a797fb610389344a03548c5bf797b3c
natebuwalda <natebuwalda@gmail.com>
2010-12-22 16:46:16 -0600

    Nate - need to rework the game class test, getting a library path error

--- src/test/java/com/jyc/scorpios/ScorpiosGameTest.java
+++ src/test/java/com/jyc/scorpios/ScorpiosGameTest.java
@@ -1,12 +1,20 @@
 package com.jyc.scorpios;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class ScorpiosGameTest {
     private ScorpiosGame game;
 
     @Before
     public void setUp() throws Exception {
-//        game = new ScorpiosGame();
+//        game = new ScorpiosGame(false);
+    }
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
     }
 }
